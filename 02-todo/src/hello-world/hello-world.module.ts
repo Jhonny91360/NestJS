7288068bfd4ed8b0c0dc37ee6b62b4bdd04af522
Jhonny91360 <jhonnyzamsa@hotmail.com>
2025-09-02 22:54:58 -0500

    chore : install apollo studio

---
+++ 02-todo/src/hello-world/hello-world.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { HelloWorldResolver } from './hello-world.resolver';
+import { Query } from '@nestjs/graphql';
+
+@Module({
+  providers: [HelloWorldResolver],
+})
+export class HelloWorldModule {
+  @Query(() => String)
+  helloWorld(): string {
+    return 'Hello World!';
+  }
+}
