22a888ba8be869e8e737751f8068975bd7698049
Jhonny91360 <jhonnyzamsa@hotmail.com>
2025-09-03 22:35:14 -0500

    feat : first query

--- 02-todo/src/hello-world/hello-world.module.ts
+++ 02-todo/src/hello-world/hello-world.module.ts
@@ -1,13 +1,36 @@
 import { Module } from '@nestjs/common';
 import { HelloWorldResolver } from './hello-world.resolver';
-import { Query } from '@nestjs/graphql';
+import { Args, Float, Int, Query } from '@nestjs/graphql';
 
 @Module({
   providers: [HelloWorldResolver],
 })
 export class HelloWorldModule {
-  @Query(() => String)
+  @Query(() => String, {
+    description: 'This returns a string hello world',
+    name: 'helloWorldQuery',
+  })
   helloWorld(): string {
     return 'Hello World!';
+  }
+
+  @Query(() => Float, {
+    description: 'This returns a random float',
+    name: 'randomFloatQuery',
+  })
+  randomFloat(): number {
+    return Math.random();
+  }
+
+  @Query(() => Int, {
+    description:
+      'This returns an integer number between 0 and argument "to" default 6',
+    name: 'randomFromZeroTo',
+  })
+  getRandomFromZeroTo(
+    @Args('to', { type: () => Int, nullable: true, defaultValue: 6 })
+    to: number = 6,
+  ): number {
+    return Math.floor(Math.random() * to);
   }
 }
