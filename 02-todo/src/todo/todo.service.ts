3352efe90209db345130e936da2c4497b5addf9e
Jhonny91360 <jhonnyzamsa@hotmail.com>
2025-09-10 20:25:05 -0500

    feat : todo query with graphql

---
+++ 02-todo/src/todo/todo.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Todo } from './entity/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [
+    { id: 1, description: 'Piedra del alma', done: false },
+    { id: 2, description: 'Piedra del tiempo', done: true },
+    { id: 3, description: 'Piedra del poder', done: false },
+  ];
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOne(id: number): Todo {
+    const todo = this.todos.find((todo) => todo.id === id);
+
+    if (!todo) throw new NotFoundException(`Todo with id ${id} not found `);
+
+    return todo;
+  }
+}
